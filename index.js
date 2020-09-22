b852b0080de5ba4414ddfb35cb872ba9b03dddc3
Kay Holten <holtenkay@gmail.com>
2020-09-21 20:51:39 -0700

    corrects NC state code, multi-state code problem

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const apiKey = "P4jSe07xT0iE5pVCbCnF5lfnvcLpQR48IcYYcHcn";
 //set needed url to a variable
 const searchURL = "https://developer.nps.gov/api/v1/parks";
 
+
 function formatQueryParams(params) {
     console.log('formatQueryParams ran');
     //get query params ready to tack onto searchURL
     const queryItems = Object.keys(params)
-        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+        .map(key => `${key}=${(params[key])}`)
     return queryItems.join('&');
 }
 
@@ -21,26 +22,23 @@ function displayResults(responseJson) {
             `<li>
                 <a href="${responseJson.data[i].url}"><h3>${responseJson.data[i].fullName}</h3></a>
                 <p>${responseJson.data[i].description}</p>
-                <address>
-                    <p>${responseJson.data[i].addresses[0].line1}</p>
-                    <p>${responseJson.data[i].addresses[0].city}, ${responseJson.data[i].addresses[0].stateCode}, ${responseJson.data[i].addresses[0].postalCode}</p>
-                </address>
             </li>`
         )
     }
     $('#results').removeClass('hidden');
 }
 
-function getParks(allStates, maxResults) {
+function getParks(statesFormatted, maxResults) {
     //hold needed params in an array
     const params = {
         api_key: apiKey,
         limit: maxResults,
-        stateCode: allStates,
+        stateCode: statesFormatted,
     }
 
     //run params through formatQueryParams function
     const queryString = formatQueryParams(params);
+    
     //create needed url by adding queryString onto the end of searchURL
     const url = searchURL + '?' + queryString;
 
@@ -65,8 +63,15 @@ function watchForm() {
         $.each($(`input[name='states']:checked`), function(){
             allStates.push($(this).val());
         })
+        const statesFormatted = allStates.toString();
         const maxResults = $('#js-max-results').val();
-        getParks(allStates, maxResults)
+        
+        if (statesFormatted.length === 0) {
+            $('#results-list').empty();
+            $('#results-list').append(`<p>No National Parks to show.</p>`)
+        } else {
+            getParks(statesFormatted, maxResults)
+        }
     }
 )}
 
